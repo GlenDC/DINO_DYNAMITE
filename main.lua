0483f54c729d1a2ea9159cab9758d8340378b7a1
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-04-19 02:31:59 +0200

    Port componnt engine to android works and ran the simple animation test

---
+++ main.lua
@@ -0,0 +1,31 @@
+require 'lcs.engine'
+
+function love.load()
+    ANIMATION.Create("ken",{
+        Source = love.graphics.newImage("res/tmp/ken.png"),
+        CellWidth = 70,
+        CellHeight = 80,
+        Frames = { 0, 1, 2, 3 },
+        FrameRate = 8
+        })
+
+    local description = {
+        {
+            Type= "ANIMATED_SPRITE",
+            Properties = {
+                Animation = ANIMATION.Get("ken"),
+                Extent = { 256, 256 }
+            }
+        }
+    }
+
+    ENTITY(description,{400,300})
+end
+
+function love.update(dt)
+    ENGINE.Update(dt)
+end
+
+function love.draw()
+    ENGINE.Render()
+end
